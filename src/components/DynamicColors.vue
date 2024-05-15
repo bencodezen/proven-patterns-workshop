1ee1607c401f43b7ea41d180c4a7a2b86d38b78c
Ben Hong <ben@bencodezen.io>
2024-05-15 09:51:45 -0500

    feat: add state drive css dynamic color example

---
+++ src/components/DynamicColors.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const textColor = ref('#000000')
+</script>
+
+<template>
+  <h1 class="title">Hello VueConfUS 2024!</h1>
+  <input type="color" v-model="textColor" />
+</template>
+
+<style lang="scss" scoped>
+.title {
+  color: v-bind(textColor);
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,12 @@
-<script></script>
+<script setup>
+import DynamicColors from '@/components/DynamicColors.vue'
+</script>
 
 <template>
   <div>
     <h1>Home Page</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <DynamicColors />
   </div>
 </template>
 
