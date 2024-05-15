30e8ebe5df99eaaac078b7d262c5adb9449830b5
Ben Hong <ben@bencodezen.io>
2024-05-15 13:13:13 -0500

    feat: add transparent component example with script setup

---
+++ src/components/TransparentComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  inheritAttrs: false
+  // ...
+}
+</script>
+
+<script setup></script>
+
+<template>
+  <div>
+    <label :for="$attrs.for">{{ $attrs.label }}</label>
+    <input type="text" v-bind="$attrs" />
+  </div>
+</template>
