a31c2f540de5e5a674b8880d352befbe1b6d8c23
Ben Hong <ben@bencodezen.io>
2024-05-15 11:24:16 -0500

    docs: add example for namespaced components

--- src/components/todo/index.ts
+++ src/components/todo/index.ts
@@ -1,2 +1,6 @@
+// Example:
+// Vue Command Palette Example
+// https://github.com/xiaoluoboding/vue-command-palette
+
 export { default as List } from './TodoList.vue'
 export { default as Item } from './TodoListItem.vue'
