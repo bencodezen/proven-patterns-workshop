e85de4db07b46f9a190dd18737963d3e7e836e7d
Ben Hong <ben@bencodezen.io>
2025-05-19 10:08:01 -0400

    add dynamic state drive css example

---
+++ src/components/DynamicColor.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const userColor = ref('#fff')
+</script>
+
+<template>
+  <div>
+    <h1 class="title">Dynamic Color</h1>
+    <input type="color" v-model="userColor" />
+    <p>{{ userColor }}</p>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color: v-bind(userColor);
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import ConditionalList from '@/components/ConditionalList.vue'
+import DynamicColor from '@/components/DynamicColor.vue'
 </script>
 
 <template>
   <div>
     <h1>Home Page</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <ConditionalList />
+    <DynamicColor />
   </div>
 </template>
 
