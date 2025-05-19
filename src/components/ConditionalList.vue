db054ff6ba77996542564cbad3d18ce59125586c
Ben Hong <ben@bencodezen.io>
2025-05-19 09:51:45 -0400

    add example for  conditional list on v-if and v-for combo

---
+++ src/components/ConditionalList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const list = ref([
+  { id: 1, visible: true },
+  { id: 2, visible: false },
+  { id: 3, visible: true },
+])
+
+const visibleList = computed(() => {
+  return list.value.filter((item) => item.visible)
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in visibleList" :key="item.id">
+      <span>{{ item.id }}</span>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
